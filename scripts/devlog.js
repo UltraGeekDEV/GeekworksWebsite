78105109ffa1d9bca6597f03a3c881babd6daa23
Attila <hunkrekker@gmail.com>
2024-06-02 19:33:35 +0200

    Finnished devlogs 0 and 1

--- scripts/devlog.js
+++ scripts/devlog.js
@@ -4,12 +4,17 @@ const urlParams = new URLSearchParams(queryString);
 const devlogId = urlParams.get("devlogId");
 
 let devlogData = [];
+let devlogMax = 0;
+let devlogMin = 0;
 
 LoadData();
 
 async function LoadData(){
-    var json = await fetch("../SampleData.json").then(x=>x.json());
-    devlogData = json.Projects.map(x=>x.Devlogs).reduce((acc,val)=> acc.concat(val),[]).find(x => x.ID == devlogId);
+    var json = await fetch("../Data.json").then(x=>x.json());
+    let ids = json.Projects.flatMap(x => x.Devlogs.map(y => y.Id));
+    devlogMax = Math.max.apply(Math,ids);
+    devlogMin = Math.min.apply(Math,ids);
+    devlogData = json.Projects.map(x=>x.Devlogs).reduce((acc,val)=> acc.concat(val),[]).find(x => x.Id == devlogId);
     DisplayData();
 }
 
@@ -17,7 +22,10 @@ function DisplayData(){
     document.getElementById("BannerImage").innerHTML += '<img src="../'+devlogData.BannerImageName+'"></img>';
 
     var textArea = document.getElementById("TextArea");
-    
+    var NavigationBar = document.getElementById("NavigationBar");
+
+    textArea.innerHTML += '<h3>' + devlogData.Title + '</h3>';
+
     devlogData.Paragraphs.forEach(paragraph => {
         var html = '<'+paragraph.Type+'>';
         var text = paragraph.Text;
@@ -25,17 +33,35 @@ function DisplayData(){
             html += text;
         }
         else{
-            var image = '<img src="../'+paragraph.ImageName+'" style="';
+            var image = '<img src="../'+paragraph.ImageName+'"';
             if(paragraph.ImageSide == 1){
-                html += image+'float : left;"></img>'+text;
+                html += image+'style="float:left;"></img>'+text;
             }
             else if(paragraph.ImageSide == 2){
-                html += image+'float : right;"></img>'+text;
+                html += image+'style="float:right;"></img>'+text;
             }
             else{
-
+                html = image+' class = "fullWidthImage"></img>';
             }
         }
         textArea.innerHTML += html+'</'+paragraph.Type+'>';
     });
+    console.log(devlogId);
+    console.log(devlogMax);
+    console.log(devlogMin);
+    if(devlogId > parseInt(devlogMin)){
+        NavigationBar.innerHTML += '<div style="cursor: pointer;" class="leftNav" onclick="window.location = \'devlog.html?devlogId='+(parseInt(devlogId)-1)+ '\'" class="DevlogLinkLeft">Previous devlog</div>';
+    }
+    else{
+        NavigationBar.innerHTML += '<div class="sideNav"></div>';
+        
+    }
+    NavigationBar.innerHTML += '<div style="cursor: pointer;" class="centerNav" onclick="window.location = \'../\'" class="DevlogLinkMainMenu">Home Page </div>';
+    if(devlogId < parseInt(devlogMax)){
+        NavigationBar.innerHTML += '<div style="cursor: pointer;" class="rightNav" onclick="window.location = \'devlog.html?devlogId='+(parseInt(devlogId)+1)+ '\'" class="DevlogLinkRight">Next devlog</div>';
+    }
+    else{
+        NavigationBar.innerHTML += '<div class="sideNav"></div>';
+        
+    }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,23 @@
 let data  = 0;
 var slideIndex = 0;
 
-fetch("SampleData.json").then(x=>x.json()).then(x=>data = x)
+fetch("Data.json").then(x=>x.json()).then(x=>data = x)
 .then(x=>SetupData());
 
 
 function SetupData(){
-    data.HighLightImageNames.forEach(element => {
+    data.HighlightImageNames.forEach(element => {
     document.getElementById('HighlightedStoryArea').innerHTML +=
     '<img class="MainSlide" src="'+element+ '" alt="This is highlight image">';
     });
 
-    let devlogs = data.Projects.map(x=>x.Devlogs).reduce((acc,val)=> acc.concat(val),[]).slice(0,2);
+    let devlogs = data.Projects.map(x=>x.Devlogs).reduce((acc,val)=> acc.concat(val),[]);
 
     for(let i = 0;i < devlogs.length;i++){
-        let table = '<div style="cursor: pointer;" onclick="window.location = \'Pages/devlog.html?devlogId='+devlogs[i].ID+ '\'" class="DescriptionRow"><h3>'+devlogs[i].Title+'</h3>';
+        let table = '<div style="cursor: pointer;" onclick="window.location = \'Pages/devlog.html?devlogId='+devlogs[i].Id+ '\'" class="DescriptionRow"><h3>Devlog '+devlogs[i].Id+': '+devlogs[i].Title+'</h3>';
         table += '<div class="imgTd"><img src="' + devlogs[i].HighlightImageName+'"></img></div>'+
         '<div class="Description"><p>'+devlogs[i].ShortDescription+'</p></div>';
         document.getElementById('DevlogTable').innerHTML += table+'</div>';
-    }
-
-    let projects = data.Projects;
-
-    for(let i = 0;i < projects.length;i++){
-
-        let table = '<div class="DescriptionRow"><h3>'+projects[i].Title+'</h3>';
-        table += '<div class="imgTd"><img src="' + projects[i].HighlightImageName+'"></img></div>'+
-        '<div class="Description"><p>'+projects[i].ShortDescription+'</p><div>';
-        document.getElementById('ProjectTable').innerHTML += table+'</div>';
     }
 
     showSlides();
